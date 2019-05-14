fd93450f037d748a95cec841f42a36d4a8d9e199
naokiur <atomic.transfer.comp@gmail.com>
2019-05-14 21:43:19 +0900

    [WIP]Try to coding domain design.

--- src/main/kotlin/com/handslab/domainmodeling/Main.kt
+++ src/main/kotlin/com/handslab/domainmodeling/Main.kt
@@ -1,11 +1,12 @@
 package com.handslab.domainmodeling
 
-import com.handslab.domainmodeling.domain.machine.TicketMachine
+import com.handslab.domainmodeling.domain.machine.*
 import com.handslab.domainmodeling.domain.user.HeldMoney
 import com.handslab.domainmodeling.domain.user.User
 
 fun main(args: Array<String>) {
-    val machine = TicketMachine()
+    val initialTicket = Ticket(TicketName("test"), TicketPrice(1000))
+    val machine = TicketMachine(listOf(initialTicket), Money(1000000))
 
     val user = User(HeldMoney(100))
     val ticket = machine.createTicket(user.heldMoney)
--- src/main/kotlin/com/handslab/domainmodeling/domain/machine/TicketMachine.kt
+++ src/main/kotlin/com/handslab/domainmodeling/domain/machine/TicketMachine.kt
@@ -2,7 +2,7 @@ package com.handslab.domainmodeling.domain.machine
 
 import com.handslab.domainmodeling.domain.user.HeldMoney
 
-class TicketMachine {
+class TicketMachine(val ticketList: List<Ticket>, val money: Money) {
     fun createTicket(heldMoney: HeldMoney): Ticket {
         return Ticket(TicketName("test"), TicketPrice(100))
     }
